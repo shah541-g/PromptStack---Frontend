9936b7430fa45f2dea34f45b148e9de79e1b0049
ahmadcodes39 <ahmadcodes39@gmail.com>
2025-07-05 23:17:52 +0500

    Login side done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Login from "./Pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ frontend/src/Context/authContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { app } from "../FireBase/firebaseConfig.js";
+
+const auth = getAuth(app);
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe(); 
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+// Custom hook
+export const useAuth = () => useContext(AuthContext);
